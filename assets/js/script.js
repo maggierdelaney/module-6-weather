e1be8859c6b45e9adec9af46ffe84b431d8fadfa
maggierdelaney <111933814+maggierdelaney@users.noreply.github.com>
2022-10-23 16:55:44 -0500

    sixth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@ var searchForm = document.getElementById("search-form");
 var searchCity = document.getElementById("search-city");
 var searchButton = document.getElementById("search-button");
 var cityList = document.getElementById("city-list");
-var APIkey = "863372ec60a804168fdfd9bc2b9e87df2"
+var APIkey = "63372ec60a804168fdfd9bc2b9e87df2"
 var todayWeather = document.getElementById("today-weather");
 var futureWeather = document.getElementById("future-weather");
+var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + APIkey;
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
 var savedCityList = localStorage.getItem("city-list")
     ? JSON.parse(localStorage.getItem("city-list"))
@@ -26,9 +29,7 @@ var cityListItems = (text) => {
 };
 
 function getWeatherApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}';
-  
-    fetch(requestUrl)
+    fetch(queryUrl)
       .then(function (response) {
         return response.json();
       })
